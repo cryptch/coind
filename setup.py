070eaf9816287473de578274ceca1b1a21ac5fab
fukuroh01 <36234774+fukuroh01@users.noreply.github.com>
2018-05-18 17:21:07 -0300

    Add long description

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='coind',
     version='1.0.0',
     description='A bitcoind based wallet manager in Python',
+    long_description='This is a cryptocurrency wallets management using Python, ' +
+                     'compatible with all bitcoind forked daemon.\n'+
+                     'It will turn possible to execute commands like "-daemon" to "walletpassphrase"',
     license='MIT',
     author='Cryptch',
     author_email='fukuroh1@gmail.com',
